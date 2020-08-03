5d35ffe596ae9fb31cef74f00b802073e919621b
Barry <barry.p.evans@gmail.com>
2020-08-03 11:17:32 -0700

    [*] ignore a lot of garbage. [+] add vcxproj.user back in because it has the debug location...

--- external-projects/glm-proj/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/barry/Documents/SDGJ_2020/external-projects/glm-cmake
-# Build directory: C:/Users/barry/Documents/SDGJ_2020/external-projects/glm-proj
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("glm")
-subdirs("test")
--- external-projects/glm-proj/glm/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/barry/Documents/SDGJ_2020/external-projects/glm-cmake/glm
-# Build directory: C:/Users/barry/Documents/SDGJ_2020/external-projects/glm-proj/glm
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
