7691f017c4112d0c79dfb4d27a1c654d047ff2e5
Kain Shepard <kainkun100@gmail.com>
2020-08-08 14:37:58 -0700

    bar shader

--- SDGJ2020/core/ui.cpp
+++ SDGJ2020/core/ui.cpp
@@ -8,13 +8,12 @@ void UI::Init()
 	auto hypebarRenderable = pHypebar->AddComponent<Renderable>();
 	hypebarRenderable->isUI = true;
 	hypebarRenderable->SetTexture("art/HypeBar2.png");
-	hypebarRenderable->m_layerOrder = 100;
 
 
 	pHypebar->m_position = glm::vec2(0, 3);
 	pHypebar->m_scale = glm::vec2(5,5);
 	hypebarRenderable->m_layerOrder = 1;
-	hypebarRenderable->OverrideShader("shaders/test.fs");
+	hypebarRenderable->OverrideShader("shaders/stepBarUI.fs");
 }
 
 void UI::CleanUp()
