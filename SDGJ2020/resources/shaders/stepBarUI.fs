4cf998544f42c4e2e300e07aa969535cae7aacff
Kain Shepard <kainkun100@gmail.com>
2020-08-08 17:43:18 -0700

    better hype bar

--- SDGJ2020/resources/shaders/stepBarUI.fs
+++ SDGJ2020/resources/shaders/stepBarUI.fs
@@ -9,6 +9,13 @@ uniform sampler2D colorTexture;
 void main()
 {
 	vec4 TexColor = texture(colorTexture, uv);
-	float StepColor = step(userData1, TexColor.r);
-	FragColor = vec4(StepColor,StepColor,StepColor,TexColor.a);
+	float StepColor = (TexColor.r - userData1) / (0.1);
+	//float StepColor = step(userData1, TexColor.r);
+
+	vec3 BackColor = vec3(1.0,  0.07, 0.33);
+	vec3 FillColor = vec3(0.10, 0.01, 0.22);
+
+	vec3 LerpColor = mix(BackColor,FillColor,StepColor);
+
+	FragColor = vec4(LerpColor, TexColor.a);
 }
