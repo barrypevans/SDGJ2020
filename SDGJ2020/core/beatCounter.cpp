8095236de47df917a2a0b4e921338eca9f1d17f5
BrandonMRendon <69318653+BrandonMRendon@users.noreply.github.com>
2020-08-08 18:23:24 -0700

    Added BeatCounter

---
+++ SDGJ2020/core/beatCounter.cpp
@@ -0,0 +1,31 @@
+#include "beatCounter.h"
+#include "Input.h"
+#include "entity.h"
+#include "metronome.h"
+#include "time.h"
+
+void beatCounter::Init()
+{
+	beatCount = 0;
+}
+void beatCounter::Update()
+{
+	Entity* entity = (Entity*)m_entity;
+	// put movement code here
+	//get input
+
+	if (Metronome::g_pMetronome->Beat) {
+		beatCount++;
+		if (beatCount > 3) {
+			entity->m_position.x = 3.93;
+			beatCount = 0;
+		}
+		else {
+			entity->m_position.x += .725; 
+		}
+	}
+}
+void beatCounter::CleanUp()
+{
+	beatCount = 0;
+}
